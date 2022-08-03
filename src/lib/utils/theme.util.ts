17b404bdaf9a2a6b37f3e10c792dfae00b73e9a0
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-08-03 19:30:12 -0400

    feat: add more themes

--- src/lib/utils/theme.util.ts
+++ src/lib/utils/theme.util.ts
@@ -1,6 +1,37 @@
 import { parse } from 'cookie';
 
-export const themes = ['light', 'dark'] as const;
+export const themes = [
+	'light',
+	'dark',
+	'cupcake',
+	'bumblebee',
+	'emerald',
+	'corporate',
+	'synthwave',
+	'retro',
+	'cyberpunk',
+	'valentine',
+	'halloween',
+	'garden',
+	'forest',
+	'aqua',
+	'lofi',
+	'pastel',
+	'fantasy',
+	'wireframe',
+	'black',
+	'luxury',
+	'dracula',
+	'cmyk',
+	'autumn',
+	'business',
+	'acid',
+	'lemonade',
+	'night',
+	'coffee',
+	'winter'
+] as const;
+
 export type Theme = typeof themes[number];
 
 export const isTheme = (theme: unknown): theme is Theme =>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,7 +12,37 @@ const config = {
 	plugins: [forms, typography, daisyui],
 
 	daisyui: {
-		themes: ['light', 'dark']
+		themes: [
+			'light',
+			'dark',
+			'cupcake',
+			'bumblebee',
+			'emerald',
+			'corporate',
+			'synthwave',
+			'retro',
+			'cyberpunk',
+			'valentine',
+			'halloween',
+			'garden',
+			'forest',
+			'aqua',
+			'lofi',
+			'pastel',
+			'fantasy',
+			'wireframe',
+			'black',
+			'luxury',
+			'dracula',
+			'cmyk',
+			'autumn',
+			'business',
+			'acid',
+			'lemonade',
+			'night',
+			'coffee',
+			'winter'
+		]
 	}
 };
 
