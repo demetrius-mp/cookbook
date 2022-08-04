3f99c50a88b18cad306bdf7ee11b8df086ad9068
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-08-04 11:20:25 -0400

    [wip]: reusable recipe form

--- src/routes/recipes/index.svelte
+++ src/routes/recipes/index.svelte
@@ -47,6 +47,10 @@
 
 		recipes = await trpcClient().query('recipes:list');
 	}
+
+	function removeFocusFromDropdown() {
+		(document.activeElement as HTMLElement).blur();
+	}
 </script>
 
 <div class="flex sm:flex-row flex-col justify-between items-center mb-5 gap-3">
@@ -64,28 +68,70 @@
 			<li class="col-span-1 flex flex-col rounded-lg">
 				<div class="card bg-base-200 shadow-xl">
 					<div class="card-body p-5">
-						<div class="flex justify-between gap-3">
+						<div class="flex justify-between items-center gap-3">
 							<h2 class="card-title">{recipe.name}</h2>
-							<button
-								on:click={() => handleDeleteRecipe(recipe.id)}
-								type="button"
-								class="btn btn-square btn-sm"
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									class="h-6 w-6"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-									stroke-width="2"
+							<div class="dropdown dropdown-end">
+								<label for="dropdown" tabindex="0" class="btn btn-sm btn-square">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										class="h-6 w-6"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke="currentColor"
+										stroke-width="2"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+										/>
+									</svg>
+								</label>
+								<ul
+									on:click={removeFocusFromDropdown}
+									tabindex="0"
+									class="dropdown-content menu p-2 shadow bg-base-100 rounded-box"
 								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-									/>
-								</svg>
-							</button>
+									<li>
+										<button class="text-info">
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												class="h-6 w-6"
+												fill="none"
+												viewBox="0 0 24 24"
+												stroke="currentColor"
+												stroke-width="2"
+											>
+												<path
+													stroke-linecap="round"
+													stroke-linejoin="round"
+													d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+												/>
+											</svg>
+											Edit
+										</button>
+									</li>
+									<li>
+										<button on:click={() => handleDeleteRecipe(recipe.id)} class="text-error">
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												class="h-6 w-6"
+												fill="none"
+												viewBox="0 0 24 24"
+												stroke="currentColor"
+												stroke-width="2"
+											>
+												<path
+													stroke-linecap="round"
+													stroke-linejoin="round"
+													d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+												/>
+											</svg>
+											Delete
+										</button>
+									</li>
+								</ul>
+							</div>
 						</div>
 						<div class="divider my-0" />
 						<ul>
