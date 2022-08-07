34e7587fac6b4731a3b6c34d700fc77dadbfca0f
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-08-07 14:08:34 -0400

    feat: search recipes

--- src/routes/app/recipes/browse/index.svelte
+++ src/routes/app/recipes/browse/index.svelte
@@ -16,17 +16,59 @@
 	import type { Load } from '@sveltejs/kit';
 	import trpcClient, { type InferQueryOutput } from '$lib/trpcClient';
 	import ListRecipes from '$lib/components/ListRecipes/ListRecipes.svelte';
+	import { createForm } from 'svelte-forms-lib';
 
 	export let recipes: InferQueryOutput<'recipes:list'>;
+
+	const { form, isSubmitting, handleSubmit } = createForm({
+		initialValues: {
+			query: ''
+		},
+		onSubmit: async (values) => {
+			recipes = await trpcClient().query('recipes:list', {
+				filterByCurrentUser: false,
+				filterByField: 'name',
+				query: values.query
+			});
+		}
+	});
 </script>
 
-<div class="flex sm:flex-row flex-col justify-between items-center mb-5 gap-3">
+<div class="flex flex-col justify-between items-start mb-5 gap-3">
 	<div>
 		<h3 class="text-4xl font-bold">Browsing recipes</h3>
 		<small class="text-sm">
 			<a class="link" href="/app/recipes">Browse my recipes.</a>
 		</small>
 	</div>
+	<form on:submit={handleSubmit} class="w-full">
+		<div class="form-control">
+			<div class="input-group">
+				<input
+					type="search"
+					bind:value={$form.query}
+					placeholder="Search…"
+					class="input input-bordered w-full"
+				/>
+				<button type="submit" class:loading={$isSubmitting} class="btn btn-square">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+						/>
+					</svg>
+				</button>
+			</div>
+		</div>
+	</form>
 </div>
 
 <ListRecipes {recipes} viewType="browsing" />
