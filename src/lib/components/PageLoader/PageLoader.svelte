ac0e60301d9e8628374818e197eb07733d6c7ed5
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-08-11 12:50:00 -0400

    feat: add loading page indicator

---
+++ src/lib/components/PageLoader/PageLoader.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
+	import { navigating } from '$app/stores';
+	import { onDestroy } from 'svelte';
+
+	const progress = tweened(0, { easing: cubicOut });
+	const opacity = tweened(1, { easing: cubicOut });
+
+	const unsubscribe = navigating.subscribe((v) => {
+		if (v) {
+			opacity.set(1, { duration: 0 });
+			progress.set(0.7, { duration: 3500 });
+		} else {
+			const duration = 1000;
+			progress.set(1, { duration });
+			opacity.set(0, { duration: duration / 2, delay: duration / 2 });
+			setTimeout(() => {
+				progress.set(0, { duration: 0 });
+			}, duration);
+		}
+	});
+
+	onDestroy(() => unsubscribe());
+</script>
+
+<div class="fixed top-0 left-0 right-0 h-2 z-[99] pointer-events-none" style:opacity={$opacity}>
+	<div class="h-full bg-primary" style:width={`${$progress * 100}%`} />
+</div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import { afterNavigate } from '$app/navigation';
-
 	import { page, session } from '$app/stores';
 	import Container from '$lib/components/Container/Container.svelte';
 	import DrawerContent from '$lib/components/Drawer/DrawerContent.svelte';
 	import DrawerLayout from '$lib/components/Drawer/DrawerLayout.svelte';
 	import Navbar from '$lib/components/Navbar/Navbar.svelte';
+	import PageLoader from '$lib/components/PageLoader/PageLoader.svelte';
 	import toastStore from '$lib/components/Toast/toast.store';
 	import ToastContainer from '$lib/components/Toast/ToastContainer.svelte';
 	import qs from 'query-string';
@@ -33,6 +33,11 @@
 <ToastContainer class="toast-end toast-bottom" />
 
 <div data-theme={$session.theme}>
+	{#if true}
+		<div>
+			<PageLoader />
+		</div>
+	{/if}
 	<DrawerLayout>
 		<svelte:fragment slot="page content">
 			<Navbar />
